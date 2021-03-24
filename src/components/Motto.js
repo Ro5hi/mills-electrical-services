49511bb3e76a75854367a087a5b7468cd727cdb9
= <=>
2021-03-24 12:22:51 -0600

    added motto, WIP nav bar

---
+++ src/components/Motto.js
@@ -0,0 +1,14 @@
+const Motto = () => {
+    return (
+        <div className="">
+            <p>
+                ✓ QUALITY  <br />
+                ✓ SAFETY   <br />   
+                ✓ REASONABLE RATES <br />
+                ✓ 100% SATISFACTION GUARANTEED <br />
+            </p>
+        </div>
+    );
+};
+
+export default Motto;
---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+// import React, { useState, useEffect } from 'react'
+
+const Nav = () => {
+
+    const [ scroll, setScroll ] = useState();
+
+    return (
+        <div className="">
+            <div className="">
+                <div className="">
+                    <button className="">Home</button>
+                    <button className="">About</button>
+                    <button className="">Contact</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
