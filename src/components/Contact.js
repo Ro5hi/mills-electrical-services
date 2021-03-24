f542253b8dbd4f8a407829d80669ca6a86f204ca
= <=>
2021-03-24 12:09:08 -0600

    added contact and about

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+const Contact = () => {
+    return (
+        <div className="">
+            <p className="">
+                Services are priced at reasonable rates, contact for an appointment today!
+            </p>
+            <div className="">
+                <p>
+                    (765) 215-7300
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
