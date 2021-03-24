9fa87535a9ddb0af5c2cca5f80aa7456078f3b1e
= <=>
2021-03-24 11:51:34 -0600

    added components, installed tailwindcss

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="text-s">
+            <p class="tracking-tighter">
+                Mills Electrical Services 2021
+            </p>
+        </div>
+    )
+}
