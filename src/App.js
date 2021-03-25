9580d8ae3f164a05a6bd06a72fc4f73d1f043217
= <=>
2021-03-25 11:21:17 -0600

    changed footer styling

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './styles.css'
 
 function App() {
-
+    
   const Home = React.lazy(() => import('./components/Home'))
   const About = React.lazy(() => import('./components/About'))
   const Contact = React.lazy(() => import('./components/Contact'))
   
   return (
-    <div className="">
-      <Suspense fallback={""}>
-      <BrowserRouter>
-        <Switch>
-          <Route component={Home} path='/' exact />
-          <Route component={About} path='/about' exact />
-          <Route component={Contact} path='/contact' exact />
-        </Switch>
-      </BrowserRouter>
-      </Suspense>
-    </div>
+    <>
+      <div className="">
+        <Suspense fallback={""}>
+        <BrowserRouter>
+          <Switch>
+            <Route component={Home} path='/' exact />
+            <Route component={About} path='/about' exact />
+            <Route component={Contact} path='/contact' exact />
+          </Switch>
+        </BrowserRouter>
+        </Suspense>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className="bg-blue-900 ">
             <div className="flex justify-center items-center h-16">
                 <p class="tracking-widest text-center text-lg text-white">
-                    Mills Electrical Services 2021
+                    Mills Electrical Services © 2021
                 </p>
             </div>
         </div>
