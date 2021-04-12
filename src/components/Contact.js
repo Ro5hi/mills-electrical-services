ee2338621e8c5bab30ad7cf733d3fd7948d14645
= <=>
2021-04-12 14:04:38 -0600

    added tel:to on contact number

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
             <div className="">
                 <p class="text-6xl text-center pt-12 pb-24 sm:text-xs md:text-base lg:text-lg xl:text-2xl 2xl:text-xl 3xl:text-3xl 4xl:text-6xl">
                     <strong>
-                        (765) 215-7300
+                        <a href="tel:+17652157300">(765) 215-7300</a>
                     </strong>
                 </p>
             </div>
