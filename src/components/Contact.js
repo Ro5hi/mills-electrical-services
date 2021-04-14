41b21e50dc9c7c8772fa88b3146e8126131d73ee
= <=>
2021-04-14 15:31:06 -0600

    made text responsive on small devices

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,12 @@
 const Contact = () => {
     return (
         <div className="container mx-auto inset-24 p-5 pt-16 -pb-50 h-auto" id="contact">
-            <p class="text-4xl italic align-middle text-center sm:text-2xl md:text-base lg:text-lg xl:text-2xl 2xl:text-xl 3xl:text-3xl 4xl:text-4xl">
+            <p class="text-2xl italic align-middle text-center sm:text-lg md:text-lg lg:text-2xl xl:text-2xl 2xl:text-xl 3xl:text-3xl 4xl:text-3xl">
                 Services are priced at reasonable rates, contact for an appointment today!
             </p>
             <br/>
             <div className="">
-                <p class="text-6xl text-center pt-12 pb-24 sm:text-3xl md:text-base lg:text-lg xl:text-2xl 2xl:text-xl 3xl:text-3xl 4xl:text-6xl">
+                <p class="text-2xl text-center pt-12 pb-24 sm:text-lg md:text-lg lg:text-2xl xl:text-2xl 2xl:text-xl 3xl:text-3xl 4xl:text-4xl">
                     <strong>
                         <a href="tel:+17652157300">(765) 215-7300</a>
                     </strong>
